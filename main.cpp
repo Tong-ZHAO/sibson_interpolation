414f9da21e17a8000e8ec479bfbf10a4e5d430ce
Tong-ZHAO <zhaotong.patricia@gmail.com>
2017-05-07 14:50:53 -0700

    add question 6 and 7

--- main.cpp
+++ main.cpp
@@ -12,29 +12,48 @@ using namespace cimg_library;
 void usage()
 {
 	std::cout << "Usage: sibson n input.bmp output.bmp" << std::endl;
-	std::cout << "n: number of interpolation points (integer, >= 1)" << std::endl;
+	std::cout << "n: number of interpolation points (integer, >= 1)(useless in mode 2, 3)" << std::endl;
+  std::cout << "maximum error: the tolerance of error in the final image(between 0 and 255)(useless in mode 1,4)" << std::endl;
+  std::cout << "mode: 1 - fix number of points" << std::endl; 
+  std::cout << "      2 - add points one by one to meet the requirement of error" << std::endl;
+  std::cout << "      3 - use a hierarchic method to meet the requirement of error" << std::endl;
+  std::cout << "      4 - minimize the error when the number of point is fix." << std::endl;
 	std::cout << "input: input file in bmp format (24 bits)" << std::endl;
 	std::cout << "output: output file in bmp format (24 bits)" << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-	if (argc < 4)
+	if (argc < 6)
 	{ 
 		usage();
 		return 1;
 	}
 
 	// scan parameters
-	int nb_points = 1;
-	sscanf(argv[1], "%i", &nb_points);
+  int nb_points = 1;
+  sscanf(argv[1], "%i", &nb_points);
+	float max_error = 50.0;
+	sscanf(argv[2], "%f", &max_error);
+  int mode = 1;
+  sscanf(argv[3], "%i", &mode);
+  if(mode < 1 || mode > 4){
+    std::cout << "The mode is illegal> It should be 1, 2, 3 or 4." << std::endl;
+    return 1;
+  }
 
 	std::cout << yellow << "SIBSON INTERPOLATION" << white << std::endl;
-	std::cout << "number of points: " << nb_points << std::endl;
+  std::cout << "MODE: " << mode << std::endl;
 
-	// Sibson interpolation
-	Sibson sibson;
-	sibson.run(nb_points, argv[2], argv[3]);
+  Sibson sibson;
+  if(mode == 1 || mode == 4){
+    std::cout << "Number of points: " << nb_points << std::endl;
+    sibson.run(nb_points, mode, argv[4], argv[5]);
+  }
+  else{
+	  std::cout << "maximum error: " << max_error << std::endl;
+    sibson.run(max_error, mode, argv[4], argv[5]);
+  }
 
   return 0;
 }
--- sibson.h
+++ sibson.h
@@ -28,6 +28,7 @@ public:
 
 public:
 	bool run(const int nb_points,
+    const int mode,
 		const char *pInput,
 		const char *pOutput)
 	{
@@ -53,47 +54,43 @@ public:
 		points.push_back(Point(0.0, height - 1.0));
 		points.push_back(Point(width - 1.0, height - 1.0));
 
-		// TODO: add random points
-		// tip: use eg function ::random_int(0, width - 1) to get a random x coordinate value 
-		std::cout << "adding " << nb_points << " random points...";
-		for (int i = 0; i < nb_points; i++)
-		{
-      int x = random_int(0, width - 1);
-      int y = random_int(0, height - 1);
+    if(mode == 1 || mode == 4){
+		  // TODO: add random points
+		  // tip: use eg function ::random_int(0, width - 1) to get a random x coordinate value 
+		  std::cout << "adding " << nb_points << " random points...";
+		  for (int i = 0; i < nb_points; i++)
+	  	{
+        int x = random_int(0, width - 1);
+        int y = random_int(0, height - 1);
       
-      points.push_back(Point(x, y));
-		}
-    points.unique();
-		std::cout << "done" << std::endl;
+        points.push_back(Point(x, y));
+		  }
+      points.unique();
+		  std::cout << "done" << std::endl;
+    }
 
     int test_num = width * height - (int)points.size();
-    std::cout << test_num << " points to calculate..."<< std::endl ;
-
-		// TODO: incremental addition
-		// tip: evaluate error in RGB or grey level space
-
+    std::cout << test_num << " points to calculate..."<< std::endl;
 
     Delaunay_triangulation dt;
     // do a delaunay triangulation 
     for (std::list<Point>::iterator it = points.begin(); it != points.end(); it++)
       dt.insert(K::Point_2(it->x(),it->y()));
 
-		// interpolation
-		std::cout << "interpolating...";
-		CImg<unsigned char> output(width, height, 1, 3, 0); // color image with pixels set to 
-    
-    // Create an error image
-    CImg<unsigned char> diff(width, height, 1, 3, 0); 
+    CImg<unsigned char> diff(width, height, 1, 3, 0);
+    CImg<unsigned char> output(width, height, 1, 3, 0);
 
     float error[3] = {0.0};
-
-		for (int x = 0; x < width; x++)
-		{
-			// simple ASCII progress bar
-			std::cout << "."; 
-
-			for (int y = 0; y < height; y++)
-			{
+      
+	  // interpolation
+	  std::cout << "interpolating...";
+    
+	  for (int x = 0; x < width; x++)
+	  {
+		  // simple ASCII progress bar
+		  std::cout << "."; 
+		  for (int y = 0; y < height; y++)
+		  {
         const Point query(x, y);
 
         double r = 0.0;
@@ -115,7 +112,6 @@ public:
         }
 
         else{
-
            K::Point_2 p(x, y);
            Point_coordinate_vector coords;
            CGAL::Triple<std::back_insert_iterator<Point_coordinate_vector>,
@@ -134,10 +130,10 @@ public:
            }
 
         }
-				// write pixel color
-				output.atXY(x, y, 0) = (unsigned char)r;
-				output.atXY(x, y, 1) = (unsigned char)g;
-				output.atXY(x, y, 2) = (unsigned char)b;
+			  // write pixel color
+			  output.atXY(x, y, 0) = (unsigned char)r;
+			  output.atXY(x, y, 1) = (unsigned char)g;
+			  output.atXY(x, y, 2) = (unsigned char)b;
 
         // write error image
 
@@ -148,19 +144,172 @@ public:
         diff.atXY(x, y, 0) = (unsigned char)abs(r - input(x, y, 0));
         diff.atXY(x, y, 1) = (unsigned char)abs(g - input(x, y, 1));
         diff.atXY(x, y, 2) = (unsigned char)abs(b - input(x, y, 2));
-			}
+		  }
 		}
-		std::cout << "done" << std::endl;
+    std::cout << "done" << std::endl;
+
+    error[0] = error[0] / test_num;
+    error[1] = error[1] / test_num;
+    error[2] = error[2] / test_num;
+
+    std::cout << "The average error in R channel: " << error[0] << std::endl;
+    std::cout << "The average error in G channel: " << error[1] << std::endl;
+    std::cout << "The average error in B channel: " << error[2] << std::endl;
+
 
 		std::cout << "write image...";
 		output.save_bmp(pOutput);
     diff.save_bmp("diff.bmp");
 		std::cout << "done" << std::endl << std::endl;
 
-    std::cout << "The average error in R channel: " << error[0] / test_num << std::endl;
-    std::cout << "The average error in G channel: " << error[1] / test_num << std::endl;
-    std::cout << "The average error in B channel: " << error[2] / test_num << std::endl;
+		return true;
+	}
+	
+  bool run(const float max_error,
+    const int mode,
+		const char *pInput,
+		const char *pOutput)
+	{
+		// read input image
+		std::cout << "read input image...";
+		CImg<unsigned char> input(pInput);
+		
+		// verify image size
+		const int width = input.width();
+		const int height = input.height();
+		if (width * height == 0)
+		{
+			std::cerr << red << "failed" << white << std::endl;
+			return false;
+		}
+		std::cout << "done" << std::endl;
 
+		// generate initial points to interpolate
+		// at the four image corners
+		std::list<Point> points;
+		points.push_back(Point(0.0, 0.0));
+		points.push_back(Point(width - 1.0, 0.0));
+		points.push_back(Point(0.0, height - 1.0));
+		points.push_back(Point(width - 1.0, height - 1.0));
+
+    Delaunay_triangulation dt;
+    // do a delaunay triangulation 
+    for (std::list<Point>::iterator it = points.begin(); it != points.end(); it++)
+      dt.insert(K::Point_2(it->x(),it->y()));
+    
+    float error[3] = {256.0};
+    // Create an error image
+    CImg<unsigned char> diff(width, height, 1, 3, 0);
+    CImg<unsigned char> output(width, height, 1, 3, 0); // color image with pixels set to 
+
+    while(error[0] > max_error || error[1] > max_error || error[2] > max_error){
+
+      if(mode == 2){
+        int x = random_int(0, width - 1);
+        int y = random_int(0, height - 1);    
+        points.push_back(Point(x, y));
+        dt.insert(K::Point_2(x, y));
+        std::cout << "add one point..." << std::endl;
+      }
+      else{
+        int add_num = (int)points.size();
+        for(int i = 0; i < add_num; i++){
+          int x = random_int(0, width - 1);
+          int y = random_int(0, height - 1);
+          points.push_back(Point(x, y));
+          dt.insert(K::Point_2(x, y));
+        }
+
+        std::cout << "add "<< add_num << " points..." << std::endl;
+      }
+
+		  // interpolation
+      error[0] = 0.0;
+      error[1] = 0.0;
+      error[2] = 0.0;
+
+      int test_num = width * height - (int)points.size();
+		  std::cout << "interpolating...";
+    
+		  for (int x = 0; x < width; x++)
+		  {
+			  // simple ASCII progress bar
+			  std::cout << "."; 
+
+			  for (int y = 0; y < height; y++)
+			  {
+          const Point query(x, y);
+
+          double r = 0.0;
+          double g = 0.0;
+          double b = 0.0;
+
+          std::list<Point>::iterator itc = points.end();
+          for(std::list<Point>::iterator it = points.begin(); 
+                                       it != points.end(); 
+                                       it++){
+            if(query.coincide(*it))
+              itc = it;
+          }
+
+          if(itc != points.end()){
+            r = (double)input(x, y, 0);
+            g = (double)input(x, y, 1);
+            b = (double)input(x, y, 2);
+          }
+
+          else{
+
+             K::Point_2 p(x, y);
+             Point_coordinate_vector coords;
+             CGAL::Triple<std::back_insert_iterator<Point_coordinate_vector>,
+                          K::FT, bool> result =
+                          CGAL::natural_neighbor_coordinates_2(dt, p,
+					                std::back_inserter(coords));
+
+             for(int i = 0; i < coords.size(); i++){
+               const double w = coords[i].second / result.second;
+               const int xp = coords[i].first.x();
+               const int yp = coords[i].first.y();
+
+               r += w * (double)input(xp, yp, 0);
+               g += w * (double)input(xp, yp, 1);
+               b += w * (double)input(xp, yp, 2);
+             }
+
+          }
+				  // write pixel color
+				  output.atXY(x, y, 0) = (unsigned char)r;
+				  output.atXY(x, y, 1) = (unsigned char)g;
+				  output.atXY(x, y, 2) = (unsigned char)b;
+
+          // write error image
+
+          error[0] += abs(r - input(x, y, 0));
+          error[1] += abs(g - input(x, y, 1));
+          error[2] += abs(b - input(x, y, 2));
+
+          diff.atXY(x, y, 0) = (unsigned char)abs(r - input(x, y, 0));
+          diff.atXY(x, y, 1) = (unsigned char)abs(g - input(x, y, 1));
+          diff.atXY(x, y, 2) = (unsigned char)abs(b - input(x, y, 2));
+			  }
+		  }
+      std::cout << "done" << std::endl;
+
+      error[0] = error[0] / test_num;
+      error[1] = error[1] / test_num;
+      error[2] = error[2] / test_num;
+
+      std::cout << "The average error in R channel: " << error[0] << std::endl;
+      std::cout << "The average error in G channel: " << error[1] << std::endl;
+      std::cout << "The average error in B channel: " << error[2] << std::endl;
+
+    }
+
+		std::cout << "write image...";
+		output.save_bmp(pOutput);
+    diff.save_bmp("diff.bmp");
+		std::cout << "done" << std::endl << std::endl;
 
 		return true;
 	}
